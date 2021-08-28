97068602087e442d80be30af6f4ea274117bb514
Sourav654 <srv.muzik@yahoo.com>
2021-08-28 08:18:04 +0530

    new version|20210828

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,31 +15,27 @@ import com.example.demo.service.dao.DemoServiceDAO;
 import com.example.demo.service.impl.DemoServiceImpl;
 
 
-
 @RestController
-@RequestMapping(value = "/demo", method =RequestMethod.POST)
+@Validated
+@RequestMapping("newcall")
 public class DemoController {
-	
-
-	 Logger logger =LoggerFactory.getLogger(DemoController.class);
-	
-	@PostMapping(value ="/newResp", headers ="Accept=Application/JSON", produces="Application/JSON")
-	public MyResponse newResp(@RequestBody String myInput) {
-		MyResponse myResponse = null;
-		try {
-			DemoServiceDAO demoServiceDAO = new DemoServiceImpl();
-			logger.debug("Started getValue() method...");
-			myResponse=demoServiceDAO.getValue(myInput);
-			logger.debug("Completed getValue() method...");
-			
-		}
-		catch(Exception e){
-			logger.debug("Exception occurs",e);
-			e.printStackTrace();
-			System.out.println("Exception occurs");
-		}		
-		return myResponse;
-		
+  Logger logger =LoggerFactory.getLogger(DemoController.class);
+  
+	  @PostMapping(value ="/newResp", consumes = "application/json", produces = "application/json")
+	  public MyResponse newResp(@RequestBody String myInput) {
+	    MyResponse myResponse = null;
+	    try {
+	      DemoServiceDAO demoServiceDAO = new DemoServiceImpl();
+	      logger.debug("Started getValue() method...");
+	      myResponse=demoServiceDAO.getValue(myInput);
+	      logger.debug("Completed getValue() method...");
+	      
+	    }
+	    catch(Exception e){
+	      logger.debug("Exception occurs",e);
+	      e.printStackTrace();
+	      System.out.println("Exception occurs");
+	    }       
+	    return myResponse;
 	}
-
 }
